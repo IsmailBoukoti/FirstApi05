2540a01b3a57fdf8b93a35f7dab6dacec822ef58
Ismail Boukoti <109099866+IsmailBoukoti@users.noreply.github.com>
2023-02-09 11:17:41 +0100

    Fixed endpoints

--- src/main/java/co/develhope/firstapi5/controllers/CarController.java
+++ src/main/java/co/develhope/firstapi5/controllers/CarController.java
@@ -2,23 +2,26 @@ package co.develhope.firstapi5.controllers;
 
 import co.develhope.firstapi5.DTO.CarDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 
-    @GetMapping("/cars")
+    @GetMapping("/get")
     public CarDTO getCars(@RequestParam(required = true)String id, @RequestParam (required = true)String modelName){
         return new CarDTO(id,modelName);
     }
 
-    @PostMapping("/cars")
-    @ResponseStatus(HttpStatus.CREATED)
-    public HttpStatus postCars(@Valid @RequestBody CarDTO carDTO){
-        System.out.println(carDTO);
-        return HttpStatus.CREATED ;
+    @PostMapping("/post")
+    @ResponseBody
+    public String printCar(@Validated @RequestBody CarDTO car){
+        System.out.println(car);
+        return HttpStatus.CREATED.toString();
     }
-
 }
